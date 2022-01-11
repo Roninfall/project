d0d97afc7a808fbba4ae42c246ab18c78eb12557
Klim <gurtovij.klimentij@gmail.com>
2022-01-11 19:30:56 +0300

    Finish

--- js/movies_project.js
+++ js/movies_project.js
@@ -2,9 +2,6 @@
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const whatMovie = [];
-const whatScore = [];
-
 let personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -14,9 +11,15 @@ let personalMovieDB = {
 };
 
 for(let i = 0; i < 2; i++) {
-  whatMovie[i] = prompt('Один из последних просмотренных фильмов?', ''),
-  whatScore[i] = prompt('На сколько оцените его?', '');
-  personalMovieDB.movies[whatMovie[i]] = whatScore[i];
+  const whatMovie = prompt('Один из последних просмотренных фильмов?', ''),
+  whatScore = prompt('На сколько оцените его?', '');
+  if(whatMovie != null && whatScore != null && whatMovie != '' && whatScore != '' && whatMovie.length < 50) {
+    console.log('done');
+    personalMovieDB.movies[whatMovie] = whatScore;
+  } else {
+    console.log('error');
+    i--;
+  }
 }
 
 if(personalMovieDB.count < 10) {
