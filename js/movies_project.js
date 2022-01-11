c813dd5ccf5b45a701523818ae257be82690e5fe
Klim <gurtovij.klimentij@gmail.com>
2022-01-11 18:20:17 +0300

    add rating

--- js/movies_project.js
+++ js/movies_project.js
@@ -1,13 +1,21 @@
-const answer = prompt('Сколько фильмов вы уже посмотрели?', '');
-let numberOfFilms = answer;
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
 
 let personalMovieDB = {
-  count: answer,
+  count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
   privat: false
 };
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB.count);
+
+console.log(personalMovieDB);
